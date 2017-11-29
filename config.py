a8f4aa3c0bb2660f9b73e3d9cb12bf070f30d155
Pin Lin <moneycat711@gmail.com>
2017-11-30 01:08:37 +0800

    edit comments of config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-# kCOJ
-URL = "http://140.124.184.228:8080/Exam" # The Great kCOJ
-# kCOJ_bot
+# KCOJ's URL
+URL = "http://140.124.184.228:8080/Exam" # The Great KCOJ
+# KCOJ_bot's Detail
 TOKEN = ""
 NAME = "@kcoj_bot"
-# Admin
+# Administrators List
 ADMIN = []
